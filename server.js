42d0bcfba09b2c57228a7c4e9c3f99339081cef8
Mary Braunger <maryjezek@aol.com>
2022-02-04 09:24:24 -0500

    deploy to Heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) =>
 );
 
 // GET request for notes.html
-app.get("/api/notes", (req, res) =>
+app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
